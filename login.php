2783982e874483c3ecb5271a5ff6c5c6b22df365
Dhuruva Priyan G M <gmdhuruva@gmail.com>
2019-03-27 02:37:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,29 +3,6 @@ session_start();
 include 'dbms_server_conn.php';
 $m_no=$_GET["mobilenumber"];
 $pwd=$_GET["password"];
-$captcha;
-        if(isset($_GET['g-recaptcha-response'])){
-          $captcha=$_GET['g-recaptcha-response'];
-        }
-        if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          mysqli_close($con);
-        }
-        $secretKey = "6LeXrpUUAAAAAGBs61DVv4lHmkcl_WUvVh1Ic6ri";
-        $ip = $_SERVER['REMOTE_ADDR'];
-        // post request to server
-        $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
-        $response = file_get_contents($url);
-        $responseKeys = json_decode($response,true);
-        // should return JSON with success as true
-        if($responseKeys["success"]) {
-                
-        } else {
-			mysqli_close($con);
-			header("location:index-2.php");
-			
-			echo "authentication failed";
-        }
 $querry= "SELECT mobilenumber,password FROM user WHERE mobilenumber='$m_no' AND password='$pwd'";
 $result = mysqli_query($con,$querry);
 
